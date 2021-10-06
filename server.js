100e9d762aaa5e60b5af1e924d93645d3423ce8c
Zach Stone <zachinjapan888s@gmail.com>
2021-10-06 14:13:36 -0400

    restore server files

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-// // Require Express to run server and routes
-// const express = require("express");
-// // make a variable to run express as a function
-// const app = express();
+// Require Express to run server and routes
+const express = require("express");
+// make a variable to run express as a function
+const app = express();
 
-// // Initialize the main project folder
-// app.use(express.static("src"));
+// Initialize the main project folder
+app.use(express.static("src"));
 
-// // choose a port
-// const PORT = process.env.PORT || 3000;
-// // use express to listen to that port and run a console log if successful
-// app.listen(PORT, function () {
-//     console.log("Server is a runnin on port " + PORT);
-// })
+// choose a port
+const PORT = process.env.PORT || 3000;
+// use express to listen to that port and run a console log if successful
+app.listen(PORT, function () {
+    console.log("Server is a runnin on port " + PORT);
+})
